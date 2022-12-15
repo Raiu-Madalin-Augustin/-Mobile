080c08a13c17642b0c723e8c43eb24309e986d43
916-Raiu-Madalin <raiumadalin@yahoo.com>
2022-12-15 18:31:31 +0200

    Added try catch for deletE

--- anime_heaven_local_db/lib/my_home_page.dart
+++ anime_heaven_local_db/lib/my_home_page.dart
@@ -32,85 +32,82 @@ class _MyHomePageState extends State<MyHomePage> {
               return snapshot.data!.isEmpty
                   ? Center(child: Text('No Animes in List.'))
                   : Center(
-                child: Container(
-                  decoration: const BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topRight,
-                        end: Alignment.bottomLeft,
-                        colors: [
-                          Colors.blue,
-                          Color.fromRGBO(205, 85, 213, 296),
-                        ],
-                      )),
-                  child: ListView.builder(
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                          padding:
-                          EdgeInsets.only(left: 3, right: 3, top: 10),
-                          child: ListTile(
-                            textColor: Colors.white,
-                            onTap: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (_) =>
-                                          ModifyAnime(
-                                              snapshot.data![index])))
-                                  .then((newAnime) {
-                                if (newAnime != null) {
-                                  setState(() {
-                                    DatabaseHelper.instance
-                                        .update(newAnime);
+                      child: Container(
+                        decoration: const BoxDecoration(
+                            gradient: LinearGradient(
+                          begin: Alignment.topRight,
+                          end: Alignment.bottomLeft,
+                          colors: [
+                            Colors.blue,
+                            Color.fromRGBO(205, 85, 213, 296),
+                          ],
+                        )),
+                        child: ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                                padding:
+                                    EdgeInsets.only(left: 3, right: 3, top: 10),
+                                child: ListTile(
+                                  textColor: Colors.white,
+                                  onTap: () {
+                                    Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (_) => ModifyAnime(
+                                                    snapshot.data![index])))
+                                        .then((newAnime) {
+                                      if (newAnime != null) {
+                                        setState(() {
+                                          DatabaseHelper.instance
+                                              .update(newAnime);
 
-                                    messageResponse(
-                                        context,
-                                        newAnime.name +
-                                            " Has been modified...!");
-                                  });
-                                }
-                              });
-                            },
-                            onLongPress: () {
-                              removeAnime(context, snapshot.data![index]);
-                            },
-                            title: Text(snapshot.data![index].name),
-                            subtitle: Text(snapshot.data![index].genre),
-                            leading: CircleAvatar(
-                              backgroundColor: Colors.deepPurple[800],
-                              child: Text(
-                                  snapshot.data![index].name
-                                      .substring(0, 1),
-                                  style: const TextStyle(
-                                      color: Colors.white)),
-                            ),
-                            trailing: Text.rich(TextSpan(children: [
-                              TextSpan(
-                                  text:
-                                  "${snapshot.data![index].rating}/10",
-                                  style: const TextStyle(
-                                      color: Colors.white)),
-                              const WidgetSpan(
-                                  child: Icon(Icons.rate_review_rounded)),
-                            ])),
-                          ));
-                    },
-                  ),
-                ),
-              );
+                                          messageResponse(
+                                              context,
+                                              newAnime.name +
+                                                  " Has been modified...!");
+                                        });
+                                      }
+                                    });
+                                  },
+                                  onLongPress: () {
+                                    removeAnime(context, snapshot.data![index]);
+                                  },
+                                  title: Text(snapshot.data![index].name),
+                                  subtitle: Text(snapshot.data![index].genre),
+                                  leading: CircleAvatar(
+                                    backgroundColor: Colors.deepPurple[800],
+                                    child: Text(
+                                        snapshot.data![index].name
+                                            .substring(0, 1),
+                                        style: const TextStyle(
+                                            color: Colors.white)),
+                                  ),
+                                  trailing: Text.rich(TextSpan(children: [
+                                    TextSpan(
+                                        text:
+                                            "${snapshot.data![index].rating}/10",
+                                        style: const TextStyle(
+                                            color: Colors.white)),
+                                    const WidgetSpan(
+                                        child: Icon(Icons.rate_review_rounded)),
+                                  ])),
+                                ));
+                          },
+                        ),
+                      ),
+                    );
             }),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(context,
-              MaterialPageRoute(builder: (_) => const RegisterAnime()))
+                  MaterialPageRoute(builder: (_) => const RegisterAnime()))
               .then((newAnime) {
             if (newAnime != null) {
               DatabaseHelper.instance.add(newAnime);
               messageResponse(context, newAnime.name + " was added...!");
-              setState(() {
-              }
-              );
+              setState(() {});
             }
           });
         },
@@ -123,16 +120,19 @@ class _MyHomePageState extends State<MyHomePage> {
   removeAnime(BuildContext context, Anime anime) {
     showDialog(
         context: context,
-        builder: (_) =>
-            AlertDialog(
+        builder: (_) => AlertDialog(
               title: Text("Delete Anime"),
               content:
-              Text("Are you sure you want to delete " + anime.name + "?"),
+                  Text("Are you sure you want to delete " + anime.name + "?"),
               actions: [
                 TextButton(
                   onPressed: () {
                     setState(() {
-                      DatabaseHelper.instance.remove(anime.id!);
+                      try {
+                        DatabaseHelper.instance.remove(anime.id!);
+                      } on Exception catch (_) {
+                        rethrow;
+                      }
                       Navigator.pop(context);
                     });
                   },
@@ -163,15 +163,15 @@ class Anime {
   var numberOfEpisodesWatched;
   var rating;
 
-  Anime({this.id,
-    this.name,
-    this.genre,
-    this.numberOfSeasonsWatched,
-    this.numberOfEpisodesWatched,
-    this.rating});
+  Anime(
+      {this.id,
+      this.name,
+      this.genre,
+      this.numberOfSeasonsWatched,
+      this.numberOfEpisodesWatched,
+      this.rating});
 
-  factory Anime.fromMap(Map<String, dynamic> json) =>
-      new Anime(
+  factory Anime.fromMap(Map<String, dynamic> json) => new Anime(
         id: json['id'],
         name: json['name'],
         genre: json['genre'],
@@ -195,8 +195,7 @@ class Anime {
 messageResponse(BuildContext context, String name) {
   showDialog(
       context: context,
-      builder: (_) =>
-          AlertDialog(
+      builder: (_) => AlertDialog(
             title: const Text("Update Message...!"),
             content: Text("Anime $name"),
           ));
